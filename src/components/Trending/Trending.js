8a463f2c2187ba100c00e331d69a037501b04721
David Wesle <wesledavid1@gmail.com>
2024-04-29 20:53:13 +0200

    fixed trending section

--- src/components/Trending/Trending.js
+++ src/components/Trending/Trending.js
@@ -1,16 +1,47 @@
-import React from "react";
-import './Trending.css';
+import React, { useState, useEffect } from 'react';
+import "./Trending.css";
 import TrendingItem from "../TrendingItem/TrendingItem";
 
+import movieData from "../../data.json";
+
 const Trending = () => {
+  const [movies, setMovies] = useState([]);
+  
+  useEffect(() => {
+    // Beim Laden der Komponente, überprüfe den localStorage und setze den Zustand entsprechend
+    const storedMovies = JSON.parse(localStorage.getItem('movies'));
+    if (storedMovies) {
+      setMovies(storedMovies);
+    } else {
+      setMovies(movieData);
+    }
+  }, []);
+
+  const handleBookmarkClick = (index) => {
+    const updatedMovies = [...movies];
+    updatedMovies[index].isBookmarked = !updatedMovies[index].isBookmarked;
+    setMovies(updatedMovies);
+    // Speichere die aktualisierten Daten im localStorage
+    localStorage.setItem('movies', JSON.stringify(updatedMovies));
+  };
 
   return (
-    <div className="trending-container">
-        <h1 className="trendingitem-h1">Trending</h1>
-        
-        <TrendingItem />
-
-    </div>
+    <>
+      <h1 className="trendingitem-h1">Trending</h1>
+      <div className="trending-container">
+        {movies.map((movie, index) =>
+          // Überprüfe, ob der Film/Serie als "trending" markiert ist
+          movie.thumbnail.trending ? (
+            <TrendingItem
+              key={index}
+              movie={movie}
+              isBookmarkedProp={movie.isBookmarked}
+              onBookmarkClick={() => handleBookmarkClick(index)}
+            />
+          ) : null
+        )}
+      </div>
+    </>
   );
 };
 
